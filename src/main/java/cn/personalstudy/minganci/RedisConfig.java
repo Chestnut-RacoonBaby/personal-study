a59dcebca774016aed9b33dd32942d9aa131bac6
1118236 <qiaohaojie@sto.cn>
2024-11-23 16:13:55 +0800

    feat(敏感词过滤): 敏感词过滤相关

---
+++ src/main/java/cn/personalstudy/minganci/RedisConfig.java
@@ -0,0 +1,21 @@
+package cn.personalstudy.minganci;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+/**
+ * @author qiaohaojie
+ * @date: 2024/11/23 16:09:47
+ */
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory connectionFactory) {
+        StringRedisTemplate template = new StringRedisTemplate();
+        template.setConnectionFactory(connectionFactory);
+        return template;
+    }
+}
---
+++ src/main/java/cn/personalstudy/minganci/SensitiveWordController.java
@@ -0,0 +1,70 @@
+package cn.personalstudy.minganci;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.Set;
+
+/**
+ * @author qiaohaojie
+ * @date: 2024/11/23 16:10:22
+ */
+@RestController
+@RequestMapping("/api/sensitive")
+@Slf4j
+public class SensitiveWordController {
+
+    @Resource
+    private SensitiveWordFilter sensitiveWordFilter;
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    /**
+     * 添加敏感词
+     */
+    @PostMapping("/word")
+    public ResponseEntity<String> addSensitiveWord(@RequestBody String word) {
+        try {
+            // 添加到Redis
+            stringRedisTemplate.opsForSet().add("sensitive_words", word);
+            // 添加到过滤器
+            sensitiveWordFilter.addWordToMap(word);
+            return ResponseEntity.ok("添加成功");
+        } catch (Exception e) {
+            log.error("添加敏感词失败", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("添加失败");
+        }
+    }
+
+    /**
+     * 检查文本是否包含敏感词
+     */
+    @PostMapping("/check")
+    public ResponseEntity<Boolean> checkText(@RequestBody String text) {
+        return ResponseEntity.ok(sensitiveWordFilter.containsSensitiveWord(text));
+    }
+
+    /**
+     * 过滤敏感词
+     */
+    @PostMapping("/filter")
+    public ResponseEntity<String> filterText(@RequestBody String text) {
+        return ResponseEntity.ok(sensitiveWordFilter.filter(text));
+    }
+
+    /**
+     * 获取文本中的所有敏感词
+     */
+    @PostMapping("/find")
+    public ResponseEntity<Set<String>> findSensitiveWords(@RequestBody String text) {
+        return ResponseEntity.ok(sensitiveWordFilter.findSensitiveWords(text));
+    }
+}
