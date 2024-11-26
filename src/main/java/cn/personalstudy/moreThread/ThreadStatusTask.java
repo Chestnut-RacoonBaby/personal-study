6d16537163739ebe09d7eb53cdd6695a5923cdc1
qiaohaojie <qhj991015@163.com>
2024-11-26 22:27:34 +0800

    feat(1126)：WAITING与RUNNABLE状态的转换

--- src/main/java/cn/personalstudy/moreThread/ThreadStatusTask.java
+++ src/main/java/cn/personalstudy/moreThread/ThreadStatusTask.java
@@ -10,11 +10,15 @@ public class ThreadStatusTask {
 
     public static void main(String[] args) {
         // BLOCKED与RUNNABLE状态的转换
-        blockAndRunnable();
+//        blockAndRunnable();
+        // WAITING与RUNNABLE状态的转换 - Object.wait()
+//        waitingAndRunnableWithObject();
+        // WAITING与RUNNABLE状态的转换 - Thread.join()
+        waitingAndRunnableWithJoin();
     }
 
     /**
-     * BLOCKED与RUNNABLE状态的转换
+     * ① BLOCKED与RUNNABLE状态的转换
      */
     private static void blockAndRunnable() {
         Thread a = new Thread(new Runnable() {
@@ -57,6 +61,103 @@ public class ThreadStatusTask {
         try {
             Thread.sleep(2000L);
         } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * ② WAITING与RUNNABLE状态的转换 - Object.wait()
+     */
+    private static void waitingAndRunnableWithObject() {
+        Object object = new Object();
+        try {
+            Thread a = new Thread(() -> {
+                // 当前线程必须持有对象的锁
+                synchronized (object) {
+                    System.out.println("a抢到锁");
+                    try {
+                        System.out.println("a释放锁");
+                        object.wait(3000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("a执行完");
+                }
+            });
+
+            Thread b = new Thread(() -> {
+                // 当前线程必须持有对象的锁
+                synchronized (object) {
+                    System.out.println("b抢到锁");
+                    try {
+                        System.out.println("b释放锁");
+                        object.wait(3000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("b执行完");
+                }
+            });
+
+
+            a.start();
+            b.start();
+
+            System.out.println(a.getName() + ":" + a.getState()); // 输出？
+            System.out.println(b.getName() + ":" + b.getState()); // 输出？
+
+            /*
+             * a抢到锁
+             * a释放锁
+             * Thread-0:RUNNABLE
+             * b抢到锁
+             * b释放锁
+             * Thread-1:BLOCKED
+             * b执行完
+             * a执行完
+             *
+             * 1. 调用wait()方法之前线程必须持有对象的锁(synchronized (object))；
+             * 2. 调用wait()方法时，会释放当前的锁，直到有其他线程调用notify()/notifyAll()方法唤醒等待锁的线程，线程才能重新进入RUNNABLE状态；
+             * 注意：其他线程调用notify()方法只会唤醒单个等待锁的线程，如果有多个线程都在等待这个锁的话，唤醒的线程不确定。
+             * 同样，调用notifyAll()方法唤醒所有等待锁的线程之后，也不一定会马上把时间片分给刚才放弃锁的那个线程，具体要看系统的调度。
+             */
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * ② WAITING与RUNNABLE状态的转换 - Thread.join()
+     */
+    private static void waitingAndRunnableWithJoin() {
+        try {
+            Thread a = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    testMethod();
+                }
+            }, "a");
+
+            Thread b = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    testMethod();
+                }
+            }, "b");
+
+            a.start();
+            a.join();
+            b.start();
+
+            System.out.println(a.getName() + ":" + a.getState()); // 输出？
+            System.out.println(b.getName() + ":" + b.getState()); // 输出？
+
+            /*
+             * a:TERMINATED  线程启动之后立马调用了join()方法，main线程会等到a线程执行完毕，所以状态为TERMINATED
+             * b:TIMED_WAITING/RUNNABLE(b.start()) 这里可能是RUNNABLE(尚未进入同步方法)，也可能是TIMED_WAITING(进入了同步方法等待锁)
+             */
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
