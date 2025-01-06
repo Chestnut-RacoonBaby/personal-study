9b138a2fd1494951fb7c5bdb4c1ab28b3b3ac932
qiaohaojie <qhj991015@163.com>
2025-01-06 22:46:10 +0800

    feat(0106)：JVM的垃圾回收机制

---
+++ src/main/java/cn/personalstudy/JVM/GcTest.java
@@ -0,0 +1,12 @@
+package cn.personalstudy.JVM;
+
+/**
+ * @author: qiaohaojie
+ * @date: 2025-01-05 18:24:33
+ */
+public class GcTest {
+    public static void main(String[] args) {
+        byte[] allocation1, allocation2;
+        allocation1 = new byte[30900 * 1024];
+    }
+}
