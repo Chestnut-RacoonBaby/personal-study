46bbec0f0aafc17faad579e87b48cf28b6ff3bfe
qiaohaojie <qhj991015@163.com>
2025-05-18 16:08:09 +0800

    feat(0518)：上界限定符

---
+++ src/main/java/cn/personalstudy/base/fanshe/ExtendTest.java
@@ -0,0 +1,44 @@
+package cn.personalstudy.base.fanshe;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 上界限定符
+ *
+ * @author: qiaohaojie
+ * @date: 2025-05-18 15:52:53
+ */
+public class ExtendTest {
+
+    /**
+     * number的子类：byte、short、int、long、float、double
+     *
+     * @param list
+     * @param <T>
+     */
+    public static <T extends Number> void printNumbers(List<T> list) {
+        for (T num : list) {
+            System.out.println(num);
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Integer> integerList = new ArrayList<>();
+        integerList.add(1);
+        integerList.add(2);
+
+        List<Double> doubleList = new ArrayList<>();
+        doubleList.add(3.14);
+        doubleList.add(4.56);
+
+        // Integer和Double都是Number的子类，可以使用泛型参数T
+        printNumbers(integerList);
+        printNumbers(doubleList);
+
+        List<String> stringList = new ArrayList<>();
+        stringList.add("Hello");
+        stringList.add("World");
+//        printNumbers(stringList);
+    }
+}
