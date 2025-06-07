4da01c6a813c0f92aa7d73f4e92eb37b8185a988
qiaohaojie <qhj991015@163.com>
2025-06-07 16:13:00 +0800

    feat(0607)：测试代码push

--- src/main/java/cn/personalstudy/PersonalStudyApplication.java
+++ src/main/java/cn/personalstudy/PersonalStudyApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
+ * 启动类
+ *
  * @author: qiaohaojie
  * @date: 2024-11-18  19:12:20
  */
